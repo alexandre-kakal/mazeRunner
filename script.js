5b0161a8fcb0e925091d15cb1fcab813ebaf2ef7
alexandre-kakal <alexandre.ls.kakal@gmail.com>
2022-07-22 12:01:45 +0200

    feat : add wall, heart and treasure constraints

--- script.js
+++ script.js
@@ -7,36 +7,74 @@ const options = {
   ice: true,
 };
 
-createGrid(options);
+setHearts(3);
 
-// récupérer la position du héro à chaque tour joué
-getHeroPosition();
+function cellInteract(x, y) {
+  let currentHealth = getHearts();
+  let cellContent = getCellContent(x, y);
+  let monsterPower = getMonsterPower(x, y);
+  if (cellContent == LOOT) {
+    loot(x, y);
+  }
+  if (cellContent == HEART && currentHealth < 5) {
+    currentHealth++;
+    setHearts(currentHealth);
+  }
+  if (cellContent == MONSTER && monsterPower <= currentHealth) {
+    killMonster(x, y);
+    setHearts(currentHealth - monsterPower);
+  } else if (cellContent == MONSTER && monsterPower > currentHealth) {
+    alert("Looser");
+  }
+}
+
+createGrid(options);
 
 function goUp() {
   let x = heroPosition.x;
   let y = heroPosition.y;
+  let wall = getWalls(x, y);
+  if (wall.north) {
+    y++;
+  }
   y--;
+  cellInteract(x, y);
   setHeroPosition(x, y);
 }
 
 function goDown() {
   let x = heroPosition.x;
   let y = heroPosition.y;
+  let wall = getWalls(x, y);
+  if (wall.south) {
+    y--;
+  }
   y++;
+  cellInteract(x, y);
   setHeroPosition(x, y);
 }
 
 function goLeft() {
   let x = heroPosition.x;
   let y = heroPosition.y;
+  let wall = getWalls(x, y);
+  if (wall.west) {
+    x++;
+  }
   x--;
+  cellInteract(x, y);
   setHeroPosition(x, y);
 }
 
 function goRight() {
   let x = heroPosition.x;
   let y = heroPosition.y;
+  let wall = getWalls(x, y);
+  if (wall.east) {
+    x--;
+  }
   x++;
+  cellInteract(x, y);
   setHeroPosition(x, y);
 }
 
@@ -44,3 +82,19 @@ document.querySelector("#go-up").onclick = goUp;
 document.querySelector("#go-down").onclick = goDown;
 document.querySelector("#go-right").onclick = goRight;
 document.querySelector("#go-left").onclick = goLeft;
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+  e = e || window.event;
+
+  if (e.keyCode == "38") {
+    goUp();
+  } else if (e.keyCode == "40") {
+    goDown();
+  } else if (e.keyCode == "37") {
+    goLeft();
+  } else if (e.keyCode == "39") {
+    goRight();
+  }
+}
